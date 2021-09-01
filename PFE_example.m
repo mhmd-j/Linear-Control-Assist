4f892e048d0c41125c8c606dfa76fabe3da5bb6b
mhmd-j <56149395+mhmd-j@users.noreply.github.com>
2021-09-02 01:40:59 +0430

    Update PFE_example.m
    explanation about vpa and pretty is added

--- PFE_example.m
+++ PFE_example.m
@@ -2,5 +2,7 @@ clc
 clear
 G = tf([10],[1 5 6]);
 G_sym_rat = PFE(G);
+% use vpa to make it more readable if rational form is not simple enough
 G_pfe_num = vpa(G_sym_rat,2)
-% pretty(G_pfe_num)
+% "pretty" can also be used to make it more readable:
+pretty(G_pfe_num)
